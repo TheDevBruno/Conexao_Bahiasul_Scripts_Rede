469da095c7ad19d25e42bf39b3e9cdc1664ee92b
Bruno Silva <119824339+TheDevBruno@users.noreply.github.com>
2026-01-31 10:13:21 -0300

    Create linux.desktop.sh for Linux Mint setup
    Add installation script for Linux Mint with necessary commands.

---
+++ linux.desktop.sh
@@ -0,0 +1,10 @@
+
+### **No Linux Mint (Depois)**
+
+```bash
+cd ~/Downloads
+git clone https://github.com/TheDevBruno/Conexao_Bahiasul_Scripts_Rede.git
+cd Conexao_Bahiasul_Scripts_Rede
+sudo apt install whiptail speedtest-cli linssid -y
+chmod +x *.sh
+./tecnico-master.sh
